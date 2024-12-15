bda9d37ebf81bc788eed815346374e4d5c7898bf
黄郑睿 <Piaoztsdy@outlook.com>
2024-12-15 15:16:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	system("color 1f");
-    printf("请选择一个你想要玩的游戏(Please choise a game you want to play)\n(a/b):");
+    printf("璇烽�夋嫨涓�涓綘鎯宠鐜╃殑娓告垙(Please choise a game you want to play)\n(a/b):");
     char choise; scanf("%c", &choise);
     switch(choise){
         case 'a':
@@ -10,15 +10,15 @@ int main(){
             system("start ./source/gm.exe");
             break;
         case 'b':
-            printf("ERROR!");
-            Sleep(1200);
-            system("exit"); 
-            return 0;
-            //system("g++.exe ./sorce/wg.cpp -o ./sorce/wg.exe");
-            //system("start ./sorce/wg.cpp");
+            //printf("ERROR!");
+            //Sleep(1200);
+            //system("exit"); 
+            //return 0;
+            system("g++.exe ./source/wg.cpp -o ./sorce/wg.exe");
+            system("start ./source/wg.cpp");
             break;
         default:
-            printf("选择错误(Choise wrong)!");
+            printf("閫夋嫨閿欒(Choise wrong)!");
             break;
     }
 
